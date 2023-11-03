8985f6d5733019152129ca36f02855fa062edd30
Richard <richard@communiteq.com>
2023-11-03 12:15:43 +0000

    Initial commit

---
+++ javascripts/discourse/components/chat-button.js
@@ -0,0 +1,25 @@
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
+import { action } from "@ember/object";
+import { defaultHomepage } from "discourse/lib/utilities";
+
+export default class ChatButton extends Component {
+    @service currentUser;
+    @service chat;
+    @service router;
+    @service siteSettings;
+
+    get mustShow() {
+        // TODO check chat allowed groups
+        return (this.currentUser && this.siteSettings.chat_enabled &&
+          this.router.currentRouteName === `discovery.${defaultHomepage()}`);
+    }
+
+    @action
+    launchBotchat() {
+        this.chat.upsertDmChannelForUsernames([settings.chatbot_username])
+        .then((chatChannel) => {
+            this.router.transitionTo("chat.channel", ...chatChannel.routeModels);
+        });
+    }
+}
